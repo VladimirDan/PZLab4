818cb5a0a5bac1e4cfbb9184757983ce2575aeac
VladimirDan <vovaoil2003@gmail.com>
2023-05-28 01:20:07 +0300

    Add uninteractive user interface

--- index.js
+++ index.js
@@ -2,9 +2,30 @@
 
 
 const taskDisplay = require('./taskDisplay');
+const taskManager = require('./taskManager');
+
 
 function main() {
-  taskDisplay.displayMenu();
+    const args = process.argv.slice(2);
+    const command = args[0];
+
+    switch (command) {
+        case 'addTask':
+          const title = args[1];
+          const description = args[2];
+          const deadline = new Date(Date.parse(args[3]));
+          console.log(args[3]);
+          taskManager.addTask(title, description, deadline);
+          break;
+    
+
+    case 'showAllTasks':
+      taskDisplay.showAllTasks();
+      break;
+
+    default:
+        console.log('Невідома команда');
+    }
 }
 
 main();
