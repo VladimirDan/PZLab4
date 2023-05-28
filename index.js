bbfbc55d6bb2edfd3b1e42a24003b59679b20811
vladyatsuk <90204297+vladyatsuk@users.noreply.github.com>
2023-05-28 21:00:54 +0300

    Fix indents, refactor code from switch case to if-else syntax

--- index.js
+++ index.js
@@ -5,54 +5,42 @@ const taskManager = require('./taskManager');
 const dataStorage = require('./dataStorage');
 
 function main() {
-    const args = process.argv.slice(2);
-    const command = args[0];
+  const args = process.argv.slice(2);
+  const command = args[0];
 
-    switch (command) {
-        case 'addTask':
-            const title = args[1];
-            const description = args[2];
-            const deadline = new Date(Date.parse(args[3]));
-            taskManager.addTask(title, description, deadline);
-            break;
-
-        case 'show':
-            const taskType = args[1];
-            taskDisplay.show(taskType);
-            break;
-
-        case 'delete':
-            const deleteTaskTitle = args[1];
-            taskManager.deleteTask(deleteTaskTitle);
-            break;
-
-        case 'editTask':
-            const taskTitle = args[1];
-            const existingTask = taskManager.getTaskByTitle(taskTitle);
-            if (!existingTask) {
-                console.log('Task not found.');
-                break;
-            }
-
-            const updatedTask = {
-                title: args[2] || existingTask.title,
-                description: args[3] || existingTask.description,
-                deadline: args[4] || existingTask.deadline,
-                completed: existingTask.completed,
-                completedDate: existingTask.completedDate,
-            };
-
-            taskManager.editTask(taskTitle, updatedTask);
-            break;
-
-        case 'finish':
-            const taskToFinish = args[1];
-            taskManager.finishTask(taskToFinish);
-            break;
-
-        default:
-            console.log('Unknown command');
+  if (command === 'addTask') {
+    const title = args[1];
+    const description = args[2];
+    const deadline = new Date(Date.parse(args[3]));
+    taskManager.addTask(title, description, deadline);
+  } else if (command === 'show') {
+    const taskType = args[1];
+    taskDisplay.show(taskType);
+  } else if (command === 'delete') {
+    const title = args[1];
+    taskManager.deleteTask(title);
+  } else if (command === 'editTask') {
+    const title = args[1];
+    const existingTask = taskManager.getTaskByTitle(title);
+    if (!existingTask) {
+      console.log('Task not found.');
     }
+
+    const updatedTask = {
+      title: args[2] || existingTask.title,
+      description: args[3] || existingTask.description,
+      deadline: args[4] || existingTask.deadline,
+      completed: existingTask.completed,
+      completedDate: existingTask.completedDate,
+    };
+
+    taskManager.editTask(taskTitle, updatedTask);
+  } else if (command === 'finish') {
+    const task = args[1];
+    taskManager.finishTask(task);
+  } else {
+    console.log('Unknown command');
+  }
 }
 
 main();
